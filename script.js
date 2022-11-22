07b5c10ea93346d228886f8344f913b0a032a5f5
trananh1992 <trananh1992@gmail.com>
2022-11-22 13:12:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+(function(a,b,c,d,e,f,g,h,i){a[e]||(i=a[e]=function(){(a[e].q=a[e].q||[]).push(arguments)},i.l=1*new Date,i.o=f,
+g=b.createElement(c),h=b.getElementsByTagName(c)[0],g.async=1,g.src=d,g.setAttribute("n",e),h.parentNode.insertBefore(g,h)
+)})(window,document,"script", "https://widgets.sir.sportradar.com/123/widgetloader", "SIR", {
+    theme: false, // using custom theme
+    language: "vi"
+});
+SIR("addWidget", ".sr-widget-1", "match.lmtPlus", {showOdds:true,matchId:32986743});
